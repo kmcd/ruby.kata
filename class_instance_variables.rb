2372632db63c4e0dcde08f11d88cde128395525a
Keith McDonnell <keith@dancingtext.com>
2019-05-03 13:47:31 +0100

    Class instance variables FTW!

--- class_instance_variables.rb
+++ class_instance_variables.rb
@@ -5,4 +5,21 @@ class ClassInstanceVariables < Minitest::Test
   # @!visibility private
   def setup
   end
+
+  class Foo
+    @bar = 1
+
+    class << self
+      attr_reader :bar
+    end
+  end
+
+  class Baz < Foo
+    @bar = 2
+  end
+
+  def test_not_shared_between_subclasses
+    assert_equal 1, Foo.bar
+    assert_equal 2, Baz.bar
+  end
 end
