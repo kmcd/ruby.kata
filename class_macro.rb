37faf0e10d41995d3ac09e0954a51b625cb3b278
Keith McDonnell <keith@dancingtext.com>
2019-05-03 14:55:13 +0100

    Macros OK

--- class_macro.rb
+++ class_macro.rb
@@ -5,4 +5,22 @@ class ClassMacro < Minitest::Test
   # @!visibility private
   def setup
   end
+
+  class Foo
+    def self.macro(name, output)
+      class_eval do
+        define_method name do |arg=output|
+          puts arg
+        end
+      end
+    end
+  end
+
+  def test_generate_instance_methods
+    Foo.macro :bar, 'baz'
+    foo = Foo.new
+
+    assert_output(/baz/)  { foo.bar }
+    assert_output(/quux/) { foo.bar 'quux' }
+  end
 end
