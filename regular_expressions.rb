193291b722aad2db5ce76a8a2cf30dcf2fe7b291
Keith McDonnell <keith@dancingtext.com>
2019-05-06 15:38:41 +0100

    Regex OK

--- regular_expressions.rb
+++ regular_expressions.rb
@@ -2,39 +2,40 @@ $LOAD_PATH.unshift File.expand_path(".")
 require 'test_helper'
 
 class RegularExpressions < Minitest::Test
-  # @!visibility private
-  def setup
+  def setup # @!visibility private
   end
 
   def test_non_greedy
-    skip :pending
+    assert_equal 'foo foo ', "foo foo bar"[ /fo.*\s+/ ]
+    assert_equal 'foo ', "foo bar"[ /fo.*?\s+/ ]
   end
 
   def test_positive_look_ahead
-    skip :pending
+    assert_equal "foo", "foo bar"[/foo(?= bar)/]
   end
 
   def test_negative_look_ahead
-    skip :pending
+    assert_equal "foo", "foo bar"[/foo(?! baz)/]
   end
 
   def test_positive_look_behind
-    skip :pending
+    assert_equal "bar", "foo bar"[/(?<=foo )bar/]
   end
 
   def test_negative_look_behind
-    skip :pending
+    assert_equal "bar", "baz bar"[/(?<!foo )bar/]
+    assert_nil "foo bar"[/(?<!foo )bar/]
   end
 
   def test_multi_line_match
-    skip :pending
+    assert_equal "foo\nbar", "foo\nbar"[/.*/m]
   end
 
   def test_named_captures
-    skip :pending
-  end
+    regex = /(?<first>foo)\s+(?<second>bar)/
+    result = regex.match "foo bar"
 
-  def test_quantifiers
-    skip :pending
+    assert_equal 'foo', result[:first]
+    assert_equal 'bar', result[:second]
   end
 end
