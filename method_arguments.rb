04de553f7a5f2b7ab023c8dffc047a0389612071
Keith McDonnell <keith@dancingtext.com>
2019-05-06 13:42:25 +0100

    Method args OK

--- method_arguments.rb
+++ method_arguments.rb
@@ -6,11 +6,22 @@ class MethodArguments < Minitest::Test
   def setup
   end
 
-  def optional(a, *bar)
-    [a, bar].flatten
+  def test_optional
+    def optional(a, *bar)
+      { a:a, bar:bar }
+    end
+
+    assert_equal( { a: 1, bar: [] }, optional(1) )
   end
 
-  def test_optional
-    assert_equal [1], optional(1)
+  def test_required_after_optional
+    def optional(a, *bar, baz)
+      { a:a, bar:bar, baz: baz }
+    end
+
+    assert_raises(ArgumentError) { optional(1) }
+    assert_equal( { a: 1, bar: [], baz: 1},   optional(1, 1) )
+    assert_equal( { a: 1, bar: [], baz: 2},   optional(1, 2) )
+    assert_equal( { a: 1, bar: [2], baz: 3},  optional(1, 2, 3) )
   end
 end
